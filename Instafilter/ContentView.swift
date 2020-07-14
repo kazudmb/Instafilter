ceb8f4c9c518a3d10252619ed31d504213b98008
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-07-14 09:00:29 +0900

    add Project 13, part 3
    https://www.hackingwithswift.com/100/swiftui/64

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -10,58 +10,20 @@ import SwiftUI
 import CoreImage
 import CoreImage.CIFilterBuiltins
 
-//struct ContentView: View {
-//    @State private var image: Image?
-//
-//    var body: some View {
-//        VStack {
-//            image?
-//                .resizable()
-//                .scaledToFit()
-//        }
-//        .onAppear(perform: loadImage)
-//    }
-//
-//    func loadImage() {
-//        guard let inputImage = UIImage(named: "Example") else { return }
-//        let beginImage = CIImage(image: inputImage)
-//
-//        let context = CIContext()
-//
-////        let currentFilter = CIFilter.sepiaTone()
-////        currentFilter.inputImage = beginImage
-////        currentFilter.intensity = 1
-//
-////        let currentFilter = CIFilter.pixellate()
-////        currentFilter.inputImage = beginImage
-////        currentFilter.scale = 100
-//
-////        let currentFilter = CIFilter.crystallize()
-////        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-////        currentFilter.radius = 200
-//
-//        guard let currentFilter = CIFilter(name: "CITwirlDistortion") else { return }
-//        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-//        currentFilter.setValue(2000, forKey: kCIInputRadiusKey)
-//        currentFilter.setValue(CIVector(x: inputImage.size.width / 2, y: inputImage.size.height / 2), forKey: kCIInputCenterKey)
-//
-//        // get a CIImage from our filter or exit if that fails
-//        guard let outputImage = currentFilter.outputImage else { return }
-//
-//        // attempt to get a CGImage from our CIImage
-//        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-//            // convert that to a UIImage
-//            let uiImage = UIImage(cgImage: cgimg)
-//
-//            // and convert that to a SwiftUI image
-//            image = Image(uiImage: uiImage)
-//        }
-//    }
-//}
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save finished!")
+    }
+}
 
 struct ContentView: View {
     @State private var image: Image?
     @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
 
     var body: some View {
         VStack {
@@ -73,9 +35,18 @@ struct ContentView: View {
                self.showingImagePicker = true
             }
         }
-        .sheet(isPresented: $showingImagePicker) {
-            ImagePicker()
+        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+            ImagePicker(image: self.$inputImage)
         }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+//        UIImageWriteToSavedPhotosAlbum(inputImage, nil, nil, nil)
+        
+        let imageSaver = ImageSaver()
+        imageSaver.writeToPhotoAlbum(image: inputImage)
     }
 }
 
